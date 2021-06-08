083add8c063727a143bbe698314e073f58431512
Egor Starkov <starkov.egor@huawei.com>
2021-06-08 18:26:28 +0300

    WIP: add human

--- humans.py
+++ humans.py
@@ -5,23 +5,16 @@ import world
 from event import Emitter, Observer
 
 
-class Alarm:
-    def __init__(self, time):
-        datetime.timedelta.
-        self.time = datetime.datetime.strptime(time, '%H:%M:%S').time()
-        self.triggered = False
-
-
 class Pedant(Emitter, Observer):
     def __init__(self):
         super().__init__()
         self.logger = logging.getLogger(__class__.__name__)
         world.env.clock.attach(self)
-        self.schedule = Alarm('08:11:10')
+        self.day = world.env.clock.time.weekday()
 
     def update(self, data):
         super().update(data)
-        if data >= datetime.datetime.today() + self.schedule.time and not self.schedule.triggered:
-            self.logger.info(f' light is {data}')
-            self.schedule.triggered = True
+        if data.weekday() != self.day:
+            self.day = data.weekday()
+            print(f'{self.day=}')
         pass
